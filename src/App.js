807992e74f188417cb0632823468925beb490e60
MohammadAtikurRahman <contact.atikurrahman@gmail.com>
2022-09-12 13:06:02 +0600

    routingAdded

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './css/App.css';
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+import Home from "./components/Home";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>hello</h1> 
+      <BrowserRouter>
+          <Routes>
+            <Route path="signin" element={<Signin />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
      
      
       </header>
