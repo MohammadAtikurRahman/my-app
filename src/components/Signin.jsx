51008d271847ff1cca3a13f8c6f112a7001e820c
MohammadAtikurRahman <contact.atikurrahman@gmail.com>
2022-09-13 11:52:20 +0600

    setsignin

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -5,48 +5,50 @@ import Button from '@mui/material/Button';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormGroup from '@mui/material/FormGroup';
+import Card from '@mui/material/Card';
 
 
 export default function Signin() {
   return (
-<FormGroup> 
-    <div>
+    <FormGroup>
+      <Card variant="outlined" sx={{ pt: 4, pb: 4}}>
+        <div>
 
-      <Box
-
-    
-        component="form"
-        sx={{
-          
-
-          '& > :not(style)': { m: 1, width: '20ch' } 
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField id="outlined-basic" label="Name" variant="outlined"  size="small"/>
+          <Box
 
 
-      </Box>
-      <Box
-        component="form"
-        sx={{
-          '& > :not(style)': { m: 1, width: '20ch' }, 
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField id="outlined-basic" label="Password" variant="outlined"  size="small" />
+            component="form"
+            sx={{
 
 
-      </Box>
+              '& > :not(style)': { m: 1, width: '20ch' }
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField id="outlined-basic" label="Name" variant="outlined" size="small" />
 
-      <Button variant="contained"  sx={{ ml: 4}} >Sign in </Button>
-      <FormControlLabel   sx={{  p:2, color: 'text.secondary' } }  control={<Checkbox defaultChecked />} label="Remember me" />
 
-    </div>
+          </Box>
+          <Box
+            component="form"
+            sx={{
+              '& > :not(style)': { m: 1, width: '20ch' },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField id="outlined-basic" label="Password" variant="outlined" size="small" />
 
 
+          </Box>
+
+          <Button variant="contained" sx={{ ml: 4 }} >Sign in </Button>
+          <FormControlLabel sx={{ p: 2, color: 'text.secondary' }} control={<Checkbox defaultChecked />} label="Remember me" />
+          <Button size="small"  sx={{ display: 'block' , ml: 4}}>Forget Password</Button>
+        </div>
+           
+      </Card>
     </FormGroup>
 
 
