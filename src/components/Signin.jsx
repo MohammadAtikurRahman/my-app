8c5b46f62f7c681f2542bc9b38d9908459f84fbe
MohammadAtikurRahman <contact.atikurrahman@gmail.com>
2022-09-13 11:40:11 +0600

    signinPageAdded

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,11 +1,54 @@
-function Signin() {
-    return (
-      <div>
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormGroup from '@mui/material/FormGroup';
 
-        Signin
 
-      </div>
-    );
-  }
-  
-  export default Signin;
+export default function Signin() {
+  return (
+<FormGroup> 
+    <div>
+
+      <Box
+
+    
+        component="form"
+        sx={{
+          
+
+          '& > :not(style)': { m: 1, width: '20ch' } 
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField id="outlined-basic" label="Name" variant="outlined"  size="small"/>
+
+
+      </Box>
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '20ch' }, 
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField id="outlined-basic" label="Password" variant="outlined"  size="small" />
+
+
+      </Box>
+
+      <Button variant="contained"  sx={{ ml: 4}} >Sign in </Button>
+      <FormControlLabel   sx={{  p:2, color: 'text.secondary' } }  control={<Checkbox defaultChecked />} label="Remember me" />
+
+    </div>
+
+
+    </FormGroup>
+
+
+  );
+}
