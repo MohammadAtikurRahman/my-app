0408a46a06e9836d21279e5a6fd8558896aa4bcf
MohammadAtikurRahman <contact.atikurrahman@gmail.com>
2022-09-13 15:59:22 +0600

    codeformated

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,30 +1,19 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-
 import FormGroup from '@mui/material/FormGroup';
-
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { Card } from '@mui/material';
 import Typography from '@mui/material/Typography';
-
-
 export default function Signup() {
   return (
-
-
     <Card variant="outlined">
-
       <Box sx={{ flexGrow: 1 }}>
-
-        
         <Grid container spacing={2}>
-          
           <Grid item xs={6}>
-            
             <Box
               component="img"
               sx={{
@@ -37,26 +26,15 @@ export default function Signup() {
                 // maxWidth: { xs: 600, md: 500 },
               }}
               alt="The house from the offer."
-
-
-
               src="https://img.freepik.com/free-vector/flat-online-shopping-concept_52683-63899.jpg?w=740&t=st=1663062940~exp=1663063540~hmac=7afe75d6ac36061e9995f72e90ae3f318db59c1a2d819064be87ba727f25534d"
             />
-            
-               
-
           </Grid>
-
           <Grid item xs={6}>
-
             <FormGroup sx={{ mt: '15ch', ml: '6ch' }}>
-
               <div>
-
                 <Typography variant="h4" gutterBottom>
                   Registration
                 </Typography>
-
                 <Box
                   component="form"
                   sx={{
@@ -67,8 +45,6 @@ export default function Signup() {
                 >
                   <TextField id="outlined-basic" label="Name" variant="outlined" size="small" />
                 </Box>
-
-
                 <Box
                   component="form"
                   sx={{
@@ -79,17 +55,6 @@ export default function Signup() {
                 >
                   <TextField id="outlined-basic" label="Email" variant="outlined" size="small" />
                 </Box>
-
-
-
-
-
-
-
-
-
-
-
                 <Box
                   component="form"
                   sx={{
@@ -98,37 +63,18 @@ export default function Signup() {
                   noValidate
                   autoComplete="off"
                 >
-
-
-
-
-
-
                   <TextField id="outlined-basic" label="Password" variant="outlined" size="small" />
                 </Box>
                 <Button variant="contained" sx={{ ml: 4 }} >Sign Up </Button>
                 <FormControlLabel sx={{ p: 2, color: 'text.secondary' }} control={<Checkbox defaultChecked />} label="Remember me" />
-
               </div>
             </FormGroup>
-
-
-
-
-
-
-
-
           </Grid>
         </Grid>
       </Box>
       <Typography component="div">
-     
-      <Box sx={{ textTransform: 'capitalize', m: 1 ,fontWeight: 'light'     }}>Copy Right Md. Atikur Rahman</Box>
-     
-    </Typography>
-
+        <Box sx={{ textTransform: 'capitalize', m: 1, fontWeight: 'light' }}>Copy Right Md. Atikur Rahman</Box>
+      </Typography>
     </Card>
-
   );
 }
